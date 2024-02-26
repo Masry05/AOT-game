cbc82f1d2ecf08e0e08d5afccd8ed36f9985bd56
Masry05 <mada1421995@gmail.com>
2024-02-26 21:44:35 +0200

    added weapon

---
+++ src/game/engine/weapons/WallTrap.java
@@ -0,0 +1,14 @@
+package game.engine.weapons;
+
+public class WallTrap extends Weapon {
+	private static final int WEAPON_CODE=4;
+
+	public WallTrap(int baseDamage) {
+		super(baseDamage);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+
+}
---
+++ src/game/engine/weapons/Weapon.java
@@ -0,0 +1,15 @@
+package game.engine.weapons;
+
+public abstract class Weapon {
+	private final int baseDamage;
+	
+	public Weapon(int baseDamage) {
+		this.baseDamage= baseDamage;
+	}
+	
+	
+	
+	
+	
+
+}
