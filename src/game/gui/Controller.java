d8612629c29cfbdc221153f50e1fcdb533fc4da8
Masry05 <mada1421995@gmail.com>
2024-05-10 13:54:53 +0300

    lanes

---
+++ src/game/gui/Controller.java
@@ -0,0 +1,35 @@
+package game.gui;
+
+import java.io.IOException;
+
+import game.engine.Battle;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Controller extends Application {
+	
+	private Battle battle;
+	private int numberOfLanes;
+	
+	public void start(Stage primaryStage) {
+		numberOfLanes = 3;
+		try {
+			battle = new Battle(1,0,10,3,250);
+		}
+		catch (IOException e){	
+		}
+		try{
+			GameScene game = new GameScene(battle);
+			primaryStage.setScene(game.getGame());
+			primaryStage.show();
+		}
+		catch(Exception e) {
+			System.out.println(e);
+			e.printStackTrace();
+		}
+		
+	}
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
