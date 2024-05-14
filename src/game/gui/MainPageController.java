d7617dbeb9bdadebe103c030612c0e4bd0010914
sal-tarek <sosototo427@gmail.com>
2024-05-14 16:03:33 +0300

    Latest Update

--- src/game/gui/MainPageController.java
+++ src/game/gui/MainPageController.java
@@ -2,26 +2,27 @@ package game.gui;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import game.engine.Battle;
@@ -45,32 +46,39 @@ public class MainPageController {
 	
 	private Scene game;	
 	private Battle battle;
-	public VBox dangerLevel;
-	public HBox turns; 
-	public HBox HUD;
-	public BorderPane layout;
-	ObservableList<HBox> lanes;
+	private BorderPane layout;
+	private ObservableList<HBox> lanes;
+	private int numOfLanes;
+	private ProgressBar turnBar;
 	public MainPageController() throws Exception{
 		
 	}
-	
+
 	public MainPageController(int numOfLanes, int resources, double width , double height) throws Exception{
-		this.battle = new Battle(1, 0, 220, numOfLanes, resources);
-	    layout = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
+		this.numOfLanes = numOfLanes;
+		this.battle = new Battle(1, 0, 10, numOfLanes, resources);
+		layout = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
+		game = new Scene(layout ,width , height);	
+		game.getStylesheets().add(getClass().getResource("home.css").toExternalForm());
 		ObservableList<VBox> weapons = weaponShopBuilder();
-		HBox lowBar = (HBox) layout.lookup("#turns");
 		ListView<VBox> weaponShop = (ListView<VBox>) layout.lookup("#weaponShop");
 		weaponShop.setItems(weapons);
 		ListView<HBox> lanes = (ListView<HBox>) layout.lookup("#lanes");
-		ObservableList<HBox> lane = lanesBuilder(battle);
+		ObservableList<HBox> lane = lanesBuilder();
 		ObservableList <ImageView> approachingTitans =  updateApproachingTitans();
 		HBox titanImages = (HBox)layout.lookup("#titanImages");
 		titanImages.getChildren().addAll(updateApproachingTitans());
 		titanImages.setSpacing(10);
-		addGrass(numOfLanes);
-		updateTitansInLanes();
 		lanes.setItems(lane);
-		game = new Scene(layout ,width , height);	
+		this.turnBar = (ProgressBar) layout.lookup("#turnBar");
+		turnBar.setStyle("-fx-background-color: transparent;");
+		turnBar.setStyle("-fx-border-color: #000000; -fx-border-width: 2px;");
+		updateResources();
+		updateScore();
+		updateTurns();
+		addGrass();
+		updateTitansInLanes();
+		
 	}
 	
 	private ObservableList<VBox> weaponShopBuilder() throws IOException {
@@ -93,8 +101,8 @@ public class MainPageController {
 				default:imageView = null;
 						range = "Error while getting the range";
 			}
-	        imageView.setFitWidth(100);
-	        imageView.setFitHeight(100);
+	        imageView.setFitWidth(130);
+	        imageView.setFitHeight(130);
 			Text name = new Text(weapon.getName());
 			Text price = new Text("Price: " + weapon.getPrice());
 			temp.getChildren().addAll(imageView,name,price);
@@ -106,10 +114,24 @@ public class MainPageController {
 		}
 		 return weapons;
 	}
-	private ObservableList<HBox> lanesBuilder(Battle battle) throws FileNotFoundException {
-	    lanes = FXCollections.observableArrayList();
+	private ObservableList<HBox> lanesBuilder() throws FileNotFoundException {
+		lanes = FXCollections.observableArrayList();
 		ArrayList<Lane> originalLanes = battle.getOriginalLanes();
 		HashMap<Integer,WeaponRegistry> shop = this.battle.getWeaponFactory().getWeaponShop();
+		int heightPerLane = 0;
+		int imageSize = 0;
+		int wallLength = 0;
+		if(battle.getOriginalLanes().size()==3) {
+			heightPerLane = (int) (700/3);
+			imageSize = (int)(heightPerLane/4)-22;
+			wallLength = heightPerLane-14;
+		}
+		else {
+			heightPerLane = (int) (700/5);
+			imageSize = (int)(heightPerLane/4)-22;
+			wallLength = heightPerLane-22;
+		}
+		
 		for(int i = 0;i<originalLanes.size();i++) {
 			HBox wall = new HBox();
 			GridPane weapons = new GridPane();
@@ -128,25 +150,35 @@ public class MainPageController {
 				ImageView image = new ImageView(new Image(new FileInputStream("images"+File.separator+fileName+".png")));
 		        Label amount = new Label("X0");
 		        amount.setAlignment(Pos.BASELINE_RIGHT);
-		        image.setFitWidth(20);
-		        image.setFitHeight(20);
+		        image.setFitWidth(imageSize);
+		        image.setFitHeight(imageSize);
 		        temp.getChildren().addAll(image,amount);
+		        Insets margin = new Insets(0, 0, 10, 5);
+		        VBox.setMargin(wall, margin);
 		        weapons.add(temp, 0, j-1);
 			}
-			VBox Wall = new VBox();
-			ImageView image = new ImageView(new Image(new FileInputStream("images"+File.separator+"Wall.png")));
-			image.setFitWidth(30);
-	        image.setFitHeight(100);
+			VBox wallWeaponsHealth = new VBox();
+			Rectangle wallShape = new Rectangle(50,wallLength);
+			wallShape.setFill(Color.GREY);
+			//ImageView image = new ImageView(new Image(new FileInputStream("images"+File.separator+"Wall.png")));
 			GridPane healthBar = new GridPane();
 	        int numCols = 10;
 	        for (int j = 0; j < numCols; j++) {
-	            Rectangle rect = new Rectangle(10, 10);
+	            Rectangle rect = new Rectangle(9 ,5);
 	            rect.setFill(Color.GREEN);
-	            healthBar.add(rect, j, 0); // column , row
+	            healthBar.add(rect, j, 0);
 	        }
-	        Wall.getChildren().addAll(image,healthBar);
+	        HBox wallWeapons = new HBox();
+	        wallWeapons.setSpacing(5);
+	        wallWeapons.getChildren().addAll(wallShape,weapons);
+	        wallWeaponsHealth.getChildren().addAll(wallWeapons,healthBar);
 	        GridPane lane = new GridPane();
-	        wall.getChildren().addAll(Wall,weapons,lane);
+	        wall.getChildren().addAll(wallWeaponsHealth,lane);
+	        wall.setSpacing(5);
+	        Insets margin = new Insets(5, 0, 0, 0); // top, right, bottom, left
+	        VBox.setMargin(healthBar, margin);
+	        //Insets margin3 = new Insets(0, 0, 5, 0);
+	        //VBox.setMargin(wall, margin3);
 	        lanes.add(wall);
 			}
 		return lanes;
@@ -185,64 +217,163 @@ public class MainPageController {
 		Tooltip.install(currentImage, titanName);
 		updatedTitansQueue.add(currentImage);
 		}
-		return updatedTitansQueue;
+		return updatedTitansQueue; 
 	}
-	public void addGrass(int numLanes) throws FileNotFoundException {
-		for (int i = 0; i < numLanes; i++) {//passes on every lane
+	
+	public void updateTurns() {
+		int numOfTurns = battle.getNumberOfTurns();
+		if(numOfTurns<15) {
+			Circle circle = (Circle)layout.lookup("#turnCircle");
+			circle.setFill(Color.GREEN);
+			turnBar.setStyle("-fx-accent: green;");
+		}
+		else if(numOfTurns<30) {
+			Circle circle = (Circle)layout.lookup("#turnCircle");
+			circle.setFill(Color.YELLOW);
+			turnBar.setStyle("-fx-accent: yellow;");
+		}
+		else if(numOfTurns<35) {
+			Circle circle = (Circle)layout.lookup("#turnCircle");
+			circle.setFill(Color.ORANGE);
+			turnBar.setStyle("-fx-accent: orange;");
+		}
+		else {
+			Circle circle = (Circle)layout.lookup("#turnCircle");
+			circle.setFill(Color.DARKRED);
+			turnBar.setStyle("-fx-accent: darkred;");
+		}
+		Text turns = (Text)layout.lookup("#turnsText");
+		turns.setText(numOfTurns+"");
+		 // Change color to green
+		Task<Void> task = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+
+                for (int i = 0; i <= 100; i++) {
+                    double progress = (double) numOfTurns / 35; // get turns
+                    updateProgress(progress, 1.0); // Update progress
+                }
+                return null;
+            }
+        };
+		turnBar.progressProperty().bind(task.progressProperty());
+        new Thread(task).start();
+	}
+	public void updateResources() {
+		Label resources = (Label) layout.lookup("#resourcesNum");
+		resources.setText(" : "+battle.getResourcesGathered());
+	}
+	public void updateScore() {
+		Label score = (Label) layout.lookup("#scoreNum");
+		score.setText(" : "+battle.getScore());
+	}
+	public void addGrass() throws FileNotFoundException {
+		for (int i = 0; i < numOfLanes; i++) {//passes on every lane
 			HBox temp = lanes.get(i);
-			GridPane currentLane = (GridPane) temp.getChildren().get(2);
+			GridPane currentLane = (GridPane) temp.getChildren().get(1);
 		  for (int j = 0; j < 29; j++) {// adds each cell
-			 // System.out.println(j);// enters all
-			  ImageView grass = new ImageView(new Image(new FileInputStream("images"+File.separator+"grass.jpg")));
+			  Rectangle tempp = new Rectangle();
 			  StackPane cell = new StackPane();
-			  cell.prefHeight(150);
-			  cell.setPrefWidth(35);
-			  grass.setFitHeight(150);
-			  grass.setFitWidth(35);
-			  cell.getChildren().add(grass);
+			  cell.setPrefWidth(39);
+			  tempp.setWidth(39);
+			  /*cell.prefHeight(numOfLanes == 3?230:130);
+			  tempp.prefHeight(numOfLanes == 3?230:130);
+			   doesnt work */
+			  if(numOfLanes == 3) {
+			  cell.prefHeight(230);
+			  tempp.setHeight(230);
+			  }
+			  else 
+			  { cell.prefHeight(130);
+			    tempp.setHeight(130);
+			  }
+			  tempp.setFill(Color.GREEN);// put transparent and fix css of stackpane or directly css of rectangle
+			  cell.getChildren().add(tempp);
 			  currentLane.add(cell, j, i);
 	        }
 		  }
-		 //System.out.print(((GridPane)lanes.get(2).getChildren().get(2)).getChildren().get(26)); the stackpane is there but no grass
 		}
 	public void updateTitansInLanes () throws FileNotFoundException {
 		//created a lane with titans
-	    Lane trial = new Lane (new Wall(200));
 		PriorityQueue <Lane> allLanes = new PriorityQueue<Lane>();
+	    Lane trial = new Lane (new Wall(200));
 		trial.addTitan(new PureTitan(1,2,3,59,10,6,7));
+		trial.addTitan(new ColossalTitan(1,2,3,59,5,6,7));
+		trial.addTitan(new ColossalTitan(1,2,3,59,5,6,7));
+		trial.addTitan(new ColossalTitan(1,2,3,59,5,6,7));
+		trial.addTitan(new ColossalTitan(1,2,3,59,5,6,7));
+		trial.addTitan(new ColossalTitan(1,2,3,59,5,6,7));
+		trial.addTitan(new ColossalTitan(1,2,3,59,5,6,7));
 		trial.addTitan(new ColossalTitan(1,2,3,58,5,6,7));
+		trial.addTitan(new ColossalTitan(1,2,3,58,5,6,7));
+		Lane trial2 = new Lane (new Wall(200));
+		trial2.getLaneWall().setCurrentHealth(-5);
+		trial2.addTitan(new PureTitan(1,2,3,59,10,6,7));
+		trial2.addTitan(new ColossalTitan(1,2,3,59,5,6,7));
+		trial2.addTitan(new ColossalTitan(1,2,3,59,5,6,7));
+		trial2.addTitan(new ColossalTitan(1,2,3,59,5,6,7));
+		trial2.addTitan(new ColossalTitan(1,2,3,59,5,6,7));
+		trial2.addTitan(new ColossalTitan(1,2,3,59,5,6,7));
+		trial2.addTitan(new ColossalTitan(1,2,3,59,5,6,7));
+		trial2.addTitan(new ColossalTitan(1,2,3,59,5,6,7));
+		trial2.addTitan(new ColossalTitan(1,2,3,59,5,6,7));
+		trial2.addTitan(new ColossalTitan(1,2,3,59,5,6,7));
+		trial2.addTitan(new ColossalTitan(1,2,3,59,5,6,7));
+		trial2.addTitan(new ColossalTitan(1,2,3,59,5,6,7));
+		trial2.addTitan(new ColossalTitan(1,2,3,58,5,6,7));
+		trial2.addTitan(new ColossalTitan(1,2,3,58,5,6,7));
 		allLanes.add(trial);
-	
-	   //  PriorityQueue <Lane> allLanes = battle.getLanes(); the right way or origianl lanes to maintain order
+		allLanes.add(trial2);
+	   //  PriorityQueue <Lane> allLanes = battle.originalLanes //what should actually happen
 		Queue <Lane> temp = new LinkedList<Lane>();
 		int size1 = allLanes.size();
-	    for(int i =0; i < size1 ;i++) {
+	    for(int i = 0; i < size1 ;i++) {
 		  Lane currLane = allLanes.remove();// take one lane
 		  temp.add(currLane);
-		  PriorityQueue<Titan> currTitans = currLane.getTitans();//take the titans in this lane
+		  if(currLane.isLaneLost()) {
+			  HBox fLane = (HBox)lanes.get(i);
+			  fLane.getChildren().clear();
+			  Rectangle tempp = new Rectangle();
+			  StackPane lostLane = new StackPane();
+			  lostLane.setPrefWidth(1230);
+			  tempp.setWidth(1230);
+			  if(numOfLanes == 3) {
+			  lostLane.prefHeight(230);
+			  tempp.setHeight(230);
+			  }
+			  else 
+			  { lostLane.prefHeight(130);
+			    tempp.setHeight(130);
+			  }
+			  tempp.setFill(Color.BROWN);
+			  Label lost = new Label("This is  a Lost Lane");
+			  lostLane.getChildren().addAll(tempp,lost);
+			  fLane.getChildren().add(lostLane);
+		  }
+		  else {
+		  PriorityQueue <Titan> currTitans = currLane.getTitans();//take the titans in this lane
 		  Queue <Titan> temp2 = new LinkedList<Titan>();
 		  ImageView currentImage = null;
-		  ObservableList< Node> cellsInLane = ((GridPane)lanes.get(i).getChildren().get(2)).getChildren();// typecast into stackpane
+		  ObservableList< Node> cellsInLane = ((GridPane)lanes.get(i).getChildren().get(1)).getChildren();// typecast into stackpane
 		  int size = cellsInLane.size();
 		  for(int j = 0; j < size ;j++) {
-			  ImageView grassTemp = (ImageView)((StackPane)cellsInLane.get(j)).getChildren().remove(0);
-			  ((StackPane)cellsInLane.get(j)).getChildren().clear();
+			  StackPane curr = ((StackPane)cellsInLane.get(j));
+			  Rectangle Temp = (Rectangle)((StackPane)cellsInLane.get(j)).getChildren().remove(0);
+			  curr.getChildren().clear();
 			  VBox left = new VBox();
 			  VBox right = new VBox();
-			  StackPane curr = ((StackPane)cellsInLane.get(j));
-			  curr.getChildren().addAll(grassTemp, left, right); 
+			  curr.getChildren().addAll(Temp,left,right); 
 			  StackPane.setAlignment(right, Pos.CENTER_RIGHT);
 			  StackPane.setAlignment(left, Pos.CENTER_LEFT);
-			  //left.setMaxWidth(curr.getWidth()/2);
-			 // right.setMaxWidth(curr.getWidth()/2);
-			 // ((StackPane)cellsInLane.get(j)).getChildren().add(new Pane ());  
-		  }
+			  left.setMaxWidth(curr.getWidth()/2);
+			  right.setMaxWidth(curr.getWidth()/2);
+			  } 
 		  while(!currTitans.isEmpty()) { // each titan in lane
 			  	Titan currTitan = currTitans.remove();
 			  	temp2.add(currTitan);
 			  	int distance = currTitan.getDistance();
 			  	int col = distance/2 - 1;
-			  	
+			  	int thealth = currTitan.getCurrentHealth();
 			  if(currTitan instanceof PureTitan) 
 			      currentImage = new ImageView(new Image(new FileInputStream("images"+File.separator+"PureTitan.png")));
 			  else if (currTitan instanceof AbnormalTitan)
@@ -257,68 +388,15 @@ public class MainPageController {
 			  StackPane currCell = (StackPane) cellsInLane.get(col);
 			  int parity = distance%2;
 			  VBox willAdd =  parity == 0? (VBox)currCell.getChildren().get(1):(VBox)currCell.getChildren().get(2);
-			  
-		      //((Pane)currCell.getChildren().get(1)).getChildren().add(currentImage);
-		 
-		      //StackPane.setMargin(button1, new javafx.geometry.Insets(20, 20, 20, 20));
-		      willAdd.getChildren().add(currentImage);
+			  VBox titanCont = new VBox();
+			  Label health = new Label (""+ thealth);
+			  health.setId("healthOfTitans");
+			  titanCont.getChildren().addAll(currentImage, health);
+		      willAdd.getChildren().add(titanCont);
+		  }
 		  while(!temp2.isEmpty()) currTitans.add(temp2.remove());
-		}
-	     while (!temp.isEmpty()) allLanes.add(temp.remove());   
-	}
+		  }
+	 }
+	    while (!temp.isEmpty()) allLanes.add(temp.remove());   
   }
 }
-	/*public void movingTitansF (int numLanes) {
-		for (int i = 0; i < numLanes; i++) {//passes on every lane
-			HBox temp = lanes.get(i);
-			GridPane currentLane = (GridPane) temp.getChildren().get(2);//got one lane
-			for (int j = 0; j < 9; j++) {
-				ObservableList<Node> cells = currentLane.getChildren(); // typecast to stack pane
-		        StackPane nextCell = (StackPane)cells.get(j+1);
-		        StackPane currCell = (StackPane)cells.get(j);
-		        if(nextCell.getChildren().size() == 2) {
-		        	ImageView willAdd  = (ImageView)nextCell.getChildren().get(1);// will be a vbox
-		        	if(j==0) 
-		        		currCell.getChildren().add(willAdd);	        	
-		        	else {
-		        		if(currCell.getChildren().size()==2)
-		        	    currCell.getChildren().remove(1);
-		        	    currCell.getChildren().add(willAdd);
-	        	}
-	         }
-		        else {
-		        	if(currCell.getChildren().size()==2)
-		        	    currCell.getChildren().remove(1);
-		        }
-		        if(j==8) {
-		        	if(nextCell.getChildren().size()==2)
-		        	    nextCell.getChildren().remove(1);
-		        }
-	      }
-	   }
-    }
-	public void spawnTitans() throws FileNotFoundException {//for now spawns one titan will just use the numberoftitans per turn to loop and have 
-		//an observable list if titans and add all of them instead of one
-		// use danger levels on right to know which lane we will spawn to 
-		// take one from approaching titans and add it 
-		Titan willAdd = battle.getApproachingTitans().remove(0);//should remove from the observable list mariam made since
-		//the one we wanna add is already removed backend wise 
-		int leastDangerIndex = 0; // will be extracted 
-		HBox temp = lanes.get(leastDangerIndex);
-		GridPane leastDangerLane = (GridPane) temp.getChildren().get(2);
-		StackPane lastCell = (StackPane) leastDangerLane.getChildren().get(9);
-		ImageView titan = null;
-		 if(willAdd instanceof PureTitan)
-			   titan = new ImageView(new Image(new FileInputStream("images"+File.separator+"PureTitan.png")));
-			  else if (willAdd instanceof AbnormalTitan)
-				  titan = new ImageView(new Image(new FileInputStream("images"+File.separator+"AbnormalTitan.png")));
-			  else if (willAdd instanceof ArmoredTitan)
-				 titan = new ImageView(new Image(new FileInputStream("images"+File.separator+"ArmoredTitan.png")));
-			  else if (willAdd instanceof ColossalTitan)
-	           titan = new ImageView(new Image(new FileInputStream("images"+File.separator+"ColossalTitan.png")));
-		 titan.setFitWidth(20);
-		 titan.setFitHeight(20);
-		lastCell.getChildren().add(titan);	
-	}*/
-
-	
